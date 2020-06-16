3181f55c9166d0040d76620d78708d98bc7d6afb
jerinnishokc <jerinnishok@outlook.com>
2020-06-16 22:04:06 +0530

    Added basic routing to the project and used Higher Order Components such as withRouter from the react-router-dom library to directly get access to route props like history, match and location

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsApp = (props) => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsApp} />
+      </Switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 
-const HomePage = () => (
-  <div className="homepage">
-    <Directory />
-  </div>
-);
+const HomePage = ({ history }) => {
+  return (
+    <div className="homepage">
+      <Directory history={history} />
+    </div>
+  );
+};
 
 export default HomePage;
