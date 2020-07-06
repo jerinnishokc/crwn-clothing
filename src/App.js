e3af0577e3bb10dd77ecfb99ab67ee31d6058b48
Jerin Nishok C <31915466+jerinnishokc@users.noreply.github.com>
2020-07-06 15:13:08 +0530

    test test comment added to check codesandbox

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import Shop from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInSignUp from './pages/signIn-signUp/signIn-signUp.component';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Shop from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInSignUp from "./pages/signIn-signUp/signIn-signUp.component";
 import {
   auth,
-  createUserProfileDocument,
+  createUserProfileDocument
   //addCollectionAndDocuments,
-} from './firebase/firebase.util';
-import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/user/user.actions';
-import { getCurrentUser } from './redux/user/user.selectors';
-import { createStructuredSelector } from 'reselect';
-import Checkout from './pages/checkout/checkout.component';
+} from "./firebase/firebase.util";
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
+import { getCurrentUser } from "./redux/user/user.selectors";
+import { createStructuredSelector } from "reselect";
+import Checkout from "./pages/checkout/checkout.component";
 // import { selectCollectionsForPreview } from './redux/shop/shop.selector';
 
 class App extends React.Component {
@@ -24,16 +24,16 @@ class App extends React.Component {
     //const { setCurrentUser, collectionsArray } = this.props;
     const { setCurrentUser } = this.props;
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-        console.log('App-', userAuth);
-        userRef.onSnapshot((snapShot) => {
+        console.log("App-", userAuth);
+        userRef.onSnapshot(snapShot => {
           setCurrentUser({
             id: snapShot.id,
-            ...snapShot.data(),
+            ...snapShot.data()
           });
-          console.log('snapshot-', snapShot);
+          console.log("snapshot-", snapShot);
         });
       } else {
         setCurrentUser(userAuth);
@@ -42,6 +42,7 @@ class App extends React.Component {
       //   'collections',
       //   collectionsArray.map(({ title, items }) => ({ title, items }))
       // );
+      //test comment
     });
   }
 
@@ -73,13 +74,16 @@ class App extends React.Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: getCurrentUser,
+  currentUser: getCurrentUser
   //collectionsArray: selectCollectionsForPreview,
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
